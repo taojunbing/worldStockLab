df3341ec51862f7a8b595fba98b2763b5a542e21
taojunbing <taojunbing@outlook.com>
2026-03-12 19:33:51 +0800

    添加项目文件。

---
+++ worldStockLab.Web/Controllers/StockController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace worldStockLab.Web.Controllers
+{
+    public class StockController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ worldStockLab.Web/Views/Stock/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace worldStockLab.Web.Views.Stock
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
